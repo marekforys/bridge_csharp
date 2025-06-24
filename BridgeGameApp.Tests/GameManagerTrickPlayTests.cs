a34d3b950d3e3df2aa8c0ea1086d565828fcdbf1
Marek Forys <mforys73@gmail.com>
2025-06-24 23:23:33 +0200

    Unit tests for basic game logic
    Signed-off-by: Marek Forys <mforys73@gmail.com>

---
+++ BridgeGameApp.Tests/GameManagerTrickPlayTests.cs
@@ -0,0 +1,44 @@
+using Xunit;
+using BridgeGameApp.GameLogic;
+using BridgeGameApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BridgeGameApp.Tests
+{
+    public class GameManagerTrickPlayTests
+    {
+        [Fact]
+        public void PlayGame_Deals13Tricks_TotalTricksIs13()
+        {
+            var names = new List<string> { "North", "East", "South", "West" };
+            var manager = new GameManager(names);
+            manager.StartGame();
+            manager.PlayGame();
+            int totalTricks = manager.TricksWon.Values.Sum();
+            Assert.Equal(13, totalTricks);
+        }
+
+        [Fact]
+        public void PlayGame_TricksWonKeysMatchPlayers()
+        {
+            var names = new List<string> { "North", "East", "South", "West" };
+            var manager = new GameManager(names);
+            manager.StartGame();
+            manager.PlayGame();
+            var trickWinners = manager.TricksWon.Keys.OrderBy(x => x).ToList();
+            var playerNames = names.OrderBy(x => x).ToList();
+            Assert.Equal(playerNames, trickWinners);
+        }
+
+        [Fact]
+        public void PlayGame_HandIsEmptyAfterPlay()
+        {
+            var names = new List<string> { "North", "East", "South", "West" };
+            var manager = new GameManager(names);
+            manager.StartGame();
+            manager.PlayGame();
+            Assert.All(manager.Players, p => Assert.Empty(p.Hand));
+        }
+    }
+}
