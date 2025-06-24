b600b96f145db75981d72f39821610d2283c6900
Marek Forys <mforys73@gmail.com>
2025-06-24 22:48:26 +0200

    Correct look of player's cards hand
    Signed-off-by: Marek Forys <mforys73@gmail.com>

--- BridgeGameApp/Program.cs
+++ BridgeGameApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using BridgeGameApp.GameLogic;
+using BridgeGameApp.Models;
 
 var playerNames = new List<string> { "North", "East", "South", "West" };
 var gameManager = new GameManager(playerNames);
@@ -7,7 +8,21 @@ gameManager.StartGame();
 foreach (var player in gameManager.Players)
 {
     Console.WriteLine($"{player.Name}'s hand:");
-    foreach (var card in player.Hand)
-        Console.WriteLine(card);
+    var suits = new[] { Suit.Clubs, Suit.Diamonds, Suit.Hearts, Suit.Spades };
+    var sortedHand = player.Hand
+        .OrderBy(card => card.Suit)
+        .ThenByDescending(card => card.Rank)
+        .ToList();
+    foreach (var suit in suits)
+    {
+        var cardsInSuit = sortedHand.Where(card => card.Suit == suit).ToList();
+        if (cardsInSuit.Count > 0)
+        {
+            Console.Write($"{cardsInSuit[0].ToString().Split(' ')[0]}: "); // Print suit icon
+            foreach (var card in cardsInSuit)
+                Console.Write($"{card.ToString().Split(' ')[1]} "); // Print rank symbol
+            Console.WriteLine();
+        }
+    }
     Console.WriteLine();
 }
